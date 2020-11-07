04da7dfd984eb8a4b0cce214076c3ded741944f3
Benedikt Kopf <benedikt.kopf@tum.de>
2020-11-07 16:16:48 +0100

    Proper responds to OsmAnd requests
    First working implementation of fishes on maps

--- server/main.py
+++ server/main.py
@@ -1,3 +1,4 @@
+from osmand import *
 from flask import Flask, send_file
 app = Flask(__name__)
 
@@ -7,7 +8,10 @@ def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/osmand/<int:x>/<int:y>/<int:file>.png')
-def osmand(x, y, file):
+@app.route('/osmand/<int:zoom>/<int:x>/<int:y>.png')
+def osmand(zoom, x, y):
     # return 'testing!'
+    (lat, lon) = num2deg(x, y, zoom)
+    print(lat)
+    print(lon)
     return send_file('fish.png', attachment_filename='python.jpg')
