f6f17b6a500eb37a4582ab84808307a63e9d8125
Benedikt Kopf <benedikt.kopf@tum.de>
2020-11-07 18:05:04 +0100

    Add coordinate function

--- server/main.py
+++ server/main.py
@@ -11,7 +11,5 @@ def hello_world():
 @app.route('/osmand/<int:zoom>/<int:x>/<int:y>.png')
 def osmand(zoom, x, y):
     # return 'testing!'
-    (lat, lon) = num2deg(x, y, zoom)
-    print(lat)
-    print(lon)
+
     return send_file('fish.png', attachment_filename='python.jpg')
--- server/osmand.py
+++ server/osmand.py
@@ -1,9 +1,14 @@
 import math
 
 
-def num2deg(xtile, ytile, zoom):
+def num2deg(x, y, zoom):
     n = 2.0 ** zoom
-    lon_deg = xtile / n * 360.0 - 180.0
-    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
+    lon_deg = x / n * 360.0 - 180.0
+    lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * y / n)))
     lat_deg = math.degrees(lat_rad)
-    return (lat_deg, lon_deg)
+    return (lon_deg, lat_deg)
+
+def getTileCoordinates(x, y, zoom):
+    coords = list(num2deg(x, y, zoom)) + list(num2deg(x + 1 % 2**zoom, y + 1 % 2**zoom, zoom))
+    return coords
+
